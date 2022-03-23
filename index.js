9733990aa3b893f13393da02b42b4f6235c1e123
Loyda <palacioloydita@gmail.com>
2022-03-22 21:16:41 -0700

    commit tree 4992fada5a4d206a5c01f4fd977e076221eadd23

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    console.log(cats.push(name));
+}
+function destructivelyPrependCat(name) {
+    console.log(cats.unshift(name));
+}
+function destructivelyRemoveLastCat() {
+    console.log(cats.pop(name));
+}
+function destructivelyRemoveFirstCat() {
+    console.log(cats.shift(name));
+}
+function appendCat(name) {
+    return [...cats, name];
+}
+function prependCat(name) {
+    return [name, ...cats];
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length-1);
+}
+function removeFirstCat() {
+    return cats.slice(1, cats.length);
+}
